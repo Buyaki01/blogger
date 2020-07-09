02d74eb2f14c64ce8bbba22c03d9afa4b5d7c5f2
Buyaki <rittahbuyaki@gmail.com>
2020-07-09 14:52:42 +0300

    Add the index/new/show.html.erb,add new, show and index action in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
 end
